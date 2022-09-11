d9c202fdec4e7a445357218fd78e4a141401949e
Hoàng Anh <107913966+Hanhdeptrai11@users.noreply.github.com>
2022-09-11 16:08:51 +0700

    Create N0328B.java

---
+++ N0328B/N0328B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class N0328B
+{
+	public static void main(String[] args)
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+				{
+					System.out.print(i + " ");
+				}
+		}
+	} 
+}
